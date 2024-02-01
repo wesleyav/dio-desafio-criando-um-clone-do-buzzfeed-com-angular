3e0c9723919f90ddb983fd693873aa6c68b4c649
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2024-01-31 21:04:16 -0300

    Cria as propriedades lógicas

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./quizz.component.css'],
 })
 export class QuizzComponent {
+  title: string = '';
+  questions: any;
+  questionSelected: any;
+  answers: string[] = [];
+  answerSelected: string = '';
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+  finished: boolean = false;
+
   constructor() {}
 }
